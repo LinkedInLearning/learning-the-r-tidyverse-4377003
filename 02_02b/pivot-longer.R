672f2f9b444bec5145a3d213e58f23e3b2d28a98
charliejhadley <charliejhadley@gmail.com>
2024-03-25 19:30:57 +0000

    swap variable name

--- 02_02b/pivot-longer.R
+++ 02_02b/pivot-longer.R
@@ -3,26 +3,12 @@ library("tidyverse")
 un_data <- read_csv("data/un_data_wide.csv")
 
 
-un_data %>% 
-  pivot_longer(australia_births:sweden_births)
-
-un_data %>% 
-  pivot_longer(contains("births"))
-
-
-un_data %>% 
-  pivot_longer(contains("births"),
-               names_to = "country",
-               values_to = "births",
-               names_sep = "_")
-
-
 
 
 
 un_long %>% 
   ggplot() +
   aes(x = eom,
-      y = value,
+      y = births,
       colour = country) +
   geom_line() 
