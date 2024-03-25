3a625dffeb16b8de13c7d6f56121db4ad2193bb1
charliejhadley <charliejhadley@gmail.com>
2024-03-25 18:55:59 +0000

    add ggplot2 code

--- 02_02b/pivot-longer.R
+++ 02_02b/pivot-longer.R
@@ -2,3 +2,16 @@ library("tidyverse")
 
 un_data <- read_csv("data/un_data_wide.csv")
 
+
+
+
+
+
+
+
+un_long %>% 
+  ggplot() +
+  aes(x = eom,
+      y = value,
+      colour = country) +
+  geom_line() 
