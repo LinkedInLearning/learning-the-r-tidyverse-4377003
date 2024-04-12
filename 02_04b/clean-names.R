86b93fe761a310ca153647fe09332fb000318a29
charliejhadley <charliejhadley@gmail.com>
2024-04-12 13:09:27 +0100

    Update clean-names.R

--- 02_04b/clean-names.R
+++ 02_04b/clean-names.R
@@ -1,14 +1,4 @@
 library("tidyverse")
 
-un_data <- read_csv("data/un_data_wide.csv")
-
-
-
-
-
-un_long %>% 
-  ggplot() +
-  aes(x = eom,
-      y = births,
-      colour = country) +
-  geom_line() 
+un_data <- read_csv("data/un_data_messy.csv")
+  
