428bb65aee36ae46299dd904944e9ffdf7b1de77
charliejhadley <charliejhadley@gmail.com>
2024-04-21 11:16:12 +0100

    setup

---
+++ 04_08b/ungroup.R
@@ -0,0 +1,12 @@
+library("tidyverse")
+
+exam_data <- read_csv("data/exam-data.csv")
+
+student_year_averages <- exam_data %>% 
+  group_by(student_id, year_group) %>% 
+  summarise(avg_score = mean(c_across(contains("score")))) %>% 
+  ungroup()
+
+student_year_averages %>% 
+  filter(avg_score == max(avg_score))
+
