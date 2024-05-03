75f749530cea30ec81698f1a731e957de99bf87b
Charlie Joey Hadley <charlie@visibledata.co.uk>
2024-05-03 15:04:54 +0100

    add import code

--- 06_04b/across.R
+++ 06_04b/across.R
@@ -2,3 +2,9 @@ library("tidyverse")
 
 msleep %>% 
   select(name, contains("sleep"))
+
+
+
+# rowwise -----------------------------------------------------------------
+
+exam_data <- read_csv("data/exam-data.csv")
